2c1449b978e5b7081ccf2d59133d4b423024f519
HilalDane <hilaallaafci@gmail.com>
2021-12-16 11:15:47 +0100

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Devoloping hello feature");
+        System.out.println("GitHub Repo is already published to GitHub");
+        System.out.println("Doing more work");
 
     }
 }
